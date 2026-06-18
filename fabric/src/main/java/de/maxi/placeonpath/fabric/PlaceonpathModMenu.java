8ac5a49db750d1381facc285fc3c79bb6020cb12
Maxi <red7creeper@gmail.com>
2026-06-18 17:22:03 +0200

    Open the custom config screen from ModMenu + NeoForge

--- fabric/src/main/java/de/maxi/placeonpath/fabric/PlaceonpathModMenu.java
+++ fabric/src/main/java/de/maxi/placeonpath/fabric/PlaceonpathModMenu.java
@@ -2,12 +2,11 @@ package de.maxi.placeonpath.fabric;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import de.maxi.placeonpath.config.ModConfig;
-import me.shedaniel.autoconfig.AutoConfig;
+import de.maxi.placeonpath.client.PathConfigScreen;
 
 public class PlaceonpathModMenu implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+        return PathConfigScreen::new;
     }
 }
